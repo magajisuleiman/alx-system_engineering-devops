3c85438c8d7f3ae5e87d51b82665ec700a0c1382
magajisuleiman <magajisuleiman1203@gmail.com>
2023-09-24 23:02:02 +0100

    updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,13 @@
-#Client configuration file (w/ Puppet) 
-
+# Puppet class to manage SSH configuration
+#
+# This class ensures that the SSH configuration file for the 'ubuntu' user is present
+# and specifies settings for connecting to a remote host.
+#
+# Example usage:
+# class { 'ssh_config':
+#   ensure => present,
+#   ...
+# }
 class ssh_config {
   file { '/home/ubuntu/.ssh/config':
     ensure  => present,
@@ -7,7 +15,7 @@ class ssh_config {
     group   => 'ubuntu',
     content => @("EOF"),
       Host 3.84.239.242
-          IdentityFile ~/.ssh/school
+          IdentityFile /home/ubuntu/.ssh/school
           PasswordAuthentication no
     EOF
   }
