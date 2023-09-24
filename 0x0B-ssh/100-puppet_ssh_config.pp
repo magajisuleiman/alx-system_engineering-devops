a79cf9a04fc2e1679515a6b5ca4df3e563cb099a
magajisuleiman <magajisuleiman1203@gmail.com>
2023-09-24 22:54:23 +0100

    updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #Client configuration file (w/ Puppet) 
 
 class ssh_config {
-  file { '~/.ssh/config':
+  file { '/home/ubuntu/.ssh/config':
     ensure  => present,
     owner   => 'ubuntu',
     group   => 'ubuntu',
     content => @("EOF"),
-      Host ubuntu@3.84.239.242
+      Host 3.84.239.242
           IdentityFile ~/.ssh/school
           PasswordAuthentication no
     EOF
